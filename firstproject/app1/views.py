ce884e2950677a9175627e05edd577cadaf023ac
06vinaybellal <bellalvinay5@gmail.com>
2025-12-17 23:20:02 +0530

    initial-step

---
+++ firstproject/app1/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse,JsonResponse
+def view1(request):
+    return HttpResponse("i am view1")
+def view2(request):
+    return HttpResponse("i am view2")
+def view3(request):
+    return JsonResponse({"name":"abc","age":22})
+def dynamicview4(request):
+    qp1=request.GET.get("name")
+    return HttpResponse(f"hello {qp1}")
+
+def view5(request):
+    name=request.GET.get("name","aaa")
+    age=request.GET.get("age",22)
+    info={"name":name}
